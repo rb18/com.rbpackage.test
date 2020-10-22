49cbde828ebb401a99ea8d64ee8224ffbab9e420
Ridvan BAL <ridvan@mayadem.com>
2020-10-22 15:58:41 +0300

    Initial Package Files

---
+++ Runtime/GameUtilities.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RB.CustomPackage
+{
+    public class GameUtilities : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
